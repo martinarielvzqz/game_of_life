9dd1464d8bda3e046fa1d2496671a2a028d40553
Martin Vazquez <martin@linisco.com.ar>
2022-05-14 16:27:07 -0300

    initial commit: partial tests

---
+++ life_gui.py
@@ -0,0 +1,115 @@
+import sys
+import pygame
+from life import Life
+
+SCREEN_SIZE = 500
+FPS = 5
+
+life = Life()
+
+
+def initialize_game(pattern_file=None):
+    pygame.init()
+    screen = pygame.display.set_mode([SCREEN_SIZE, SCREEN_SIZE])
+
+    if pattern_file:
+        life.load(pattern_file)
+
+    pygame.display.set_caption(f'Game of Life [{life.rules_str()}]')
+    return screen
+
+
+def center(scale):
+    cell_count = SCREEN_SIZE // scale
+    minx, miny, maxx, maxy = life.bounding_box()
+
+    basex = minx - (cell_count - (maxx - minx + 1)) // 2
+    basey = miny - (cell_count - (maxy - miny + 1)) // 2
+    return basex, basey
+
+
+def game_loop(screen):
+    running = True
+    paused = False
+    scale = 20
+    basex, basey = center(scale)
+    interval = 1000 // FPS
+
+    while running:
+        start_time = pygame.time.get_ticks()
+
+        screen.fill((255, 255, 255))
+        for i in range(0, SCREEN_SIZE, scale):
+            pygame.draw.line(screen, (0, 0, 0), (i, 0), (i, SCREEN_SIZE))
+            pygame.draw.line(screen, (0, 0, 0), (0, i), (SCREEN_SIZE, i))
+        for cell in life.alive:
+            x = cell[0]
+            y = cell[1]
+            pygame.draw.rect(screen, (80, 80, 192),
+                             ((x - basex) * scale + 2, (y - basey) * scale + 2,
+                              scale - 3, scale - 3))
+
+        pygame.display.flip()
+        if not paused:
+            life.advance()
+
+        wait_time = 1
+        while wait_time > 0:
+            event = pygame.event.wait(timeout=wait_time)
+            while event:
+                if event.type == pygame.QUIT:
+                    running = False
+                    break
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        running = False
+                    elif event.key == pygame.K_LEFT:
+                        basex -= 2
+                    elif event.key == pygame.K_RIGHT:
+                        basex += 2
+                    elif event.key == pygame.K_UP:
+                        basey -= 2
+                    elif event.key == pygame.K_DOWN:
+                        basey += 2
+                    elif event.unicode == ' ':
+                        paused = not paused
+                        if paused:
+                            pygame.display.set_caption('Game of Life (paused)')
+                        else:
+                            pygame.display.set_caption(
+                                f'Game of Life [{life.rules_str()}]')
+                    elif event.unicode == '+':
+                        if scale < 50:
+                            scale += 5
+                    elif event.unicode == '-':
+                        if scale > 10:
+                            scale -= 5
+                    elif event.unicode == 'c':
+                        basex, basey = center(scale)
+                    break
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    mx, my = pygame.mouse.get_pos()
+                    x = mx // scale + basex
+                    y = my // scale + basey
+                    life.toggle(x, y)
+                    break
+                event = pygame.event.poll()
+            if event:
+                break
+
+            current_time = pygame.time.get_ticks()
+            wait_time = interval - (current_time - start_time)
+
+
+if __name__ == '__main__':
+    pattern_file = sys.argv[1] if len(sys.argv) > 1 else None
+    screen = initialize_game(pattern_file)
+    print('''
+Press: Arrows to scroll
+       Space to pause/resume the simulation
+       +/- to zoom in/out
+       c to re-center
+       mouse click to toggle the state of a cell
+       Esc to exit''')
+    game_loop(screen)
+    pygame.quit()
---
+++ test_life.py
@@ -0,0 +1,64 @@
+import pstats
+import unittest
+
+from life import Life, CellList
+
+
+class TestCellList(unittest.TestCase):
+    def test_empty(self):
+        c = CellList()
+        assert list(c) == []
+
+    def test_set_true(self):
+        c = CellList()
+        c.set(1, 2, True)
+        assert c.has(1, 2)
+        assert list(c) == [(1, 2)]
+        c.set(500, 600, True)
+        assert c.has(1, 2) and c.has(500, 600)
+        assert list(c) == [(1, 2), (500, 600)]
+        c.set(1, 2, True)  # make sure a cell can be set to True twice
+        assert c.has(1, 2) and c.has(500, 600)
+        assert list(c) == [(1, 2), (500, 600)]
+
+    def test_set_false(self):
+        c = CellList()
+        c.set(1, 2, False)
+        assert not c.has(1, 2)
+        assert list(c) == []
+        c.set(1, 2, True)
+        c.set(1, 2, False)
+        assert not c.has(1, 2)
+        assert list(c) == []
+        c.set(1, 2, True)
+        c.set(3, 2, True)
+        c.set(1, 2, False)
+        assert not c.has(1, 2)
+        assert c.has(3, 2)
+        assert list(c) == [(3, 2)]
+
+    def test_set_default(self):
+        c = CellList()
+        c.set(1, 2)
+        assert c.has(1, 2)
+        assert list(c) == [(1, 2)]
+        c.set(1, 2)
+        assert not c.has(1, 2)
+        assert list(c) == []
+
+class TestLife(unittest.TestCase):
+    def test_new(self):
+        life = Life()
+        assert life.survival == [2, 3]
+        assert life.birth == [3]
+        assert list(life.living_cells()) == []
+        assert life.rules_str() == '23/3'
+
+    def test_new_custom(self):
+        life = Life([3, 4], [4, 7, 8])
+        assert life.survival == [3, 4]
+        assert life.birth == [4, 7, 8]
+        assert list(life.living_cells()) == []
+        assert life.rules_str() == '34/478'
+
+
